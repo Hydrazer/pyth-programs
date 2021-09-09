877da9cb99366717fa6b7140ff78c852d469d47f
Hydrazer <explodingaerosal@gmail.com>
2021-09-09 15:38:07 -0600

    adding some stuff to big-pyth

--- zz_makeProgram/getGood.py
+++ zz_makeProgram/getGood.py
@@ -6,6 +6,9 @@ with open(r"zz_makeProgram\cool.py", "r") as file:
 with open("getGood.txt", "r") as file:
   bigContents[1] = 'r"""' + file.read() + '"""'
 
+with open("z_bigpyth.txt", "r") as file:
+  bigContents[5] = file.read()
+
 
 clipboard.copy("\n".join(bigContents))
 print("********************************\nDONE\nDONE\nDONE\n********************************")
